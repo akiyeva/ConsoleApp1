8c42960ceb00697b9fd419221cce11708e02cc30
thestarrynight17 <thestarrynight17@users.noreply.github.com>
2024-07-08 14:13:19 +0400

    TextColor class added

---
+++ ConsoleApp1/Helpers/TextColor.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleApp1.Helpers
+{
+    public static class TextColor
+    {
+        public static void WriteLine(string text, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(text);
+            Console.ResetColor();
+        }
+    }
+}
